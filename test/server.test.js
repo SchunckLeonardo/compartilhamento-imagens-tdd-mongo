b98f29aa9ad8c93fb8d34d54edf0abd33c9f0248
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-09-23 10:01:26 -0300

    Test for server

---
+++ test/server.test.js
@@ -0,0 +1,8 @@
+let app = require('../src/app')
+
+let supertest = require('supertest')
+let request = supertest(app)
+
+test("A aplicação deve rodar na porta 3131", () => {
+    return request.get('/').then(res => expect(res.statusCode).toEqual(200))
+})
